5dfc0b0587a67ba610dd85c3446fc6df7e5c0ba9
stephaniehamilton <smhamilton0208@gmail.com>
2017-06-09 19:01:02 -0400

    Simple Calculator project and tests

---
+++ SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+        [TestMethod]
+        public void ConvertsValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+            Assert.AreEqual(convertedNumber, 5);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Expected a numeric value.")]
+        public void FailsToConvertValidStringInputIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+
+        }
+    }
+}
