0ab37cd62531b56ac66902a4c9075af9b1e9ca7f
stephaniehamilton <smhamilton0208@gmail.com>
2017-06-09 00:47:00 -0400

    Add project files.

---
+++ CalculatorEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+            switch(argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+
+                case "minus":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Specified operation not recognized.");
+            }
+            return result;
+        }
+    }
+}
